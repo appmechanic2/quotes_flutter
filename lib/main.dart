f873939b9a39bf8caf00b18058cd8d3c5377df0c
appmechanic <team.mechanic2014@gmail.com>
2020-01-07 15:55:46 +0530

    Layout of quotes using cards

--- lib/main.dart
+++ lib/main.dart
@@ -12,22 +12,53 @@ class QuoteList extends StatefulWidget {
 
 class _QuoteListState extends State<QuoteList> {
   List<Quote> quotes = [
-    Quote(author: 'Raghuram',text: 'Pain is temporary Pride is forever'),
-    Quote(author: 'Ranvijay',text: 'You earn your Body'),
-    Quote(author: 'Mahatma Gandhi',text: 'No Violence')
+    Quote(author: 'Jim Rohn',text: 'Either run the day or the day runs you.'),
+    Quote(author: 'Walt Disney',text: 'The way to get started is to quit talking and begin doing.'),
+    Quote(author: 'Jim Cathcart',text: 'Become the person who would attract the results you seek'),
+    Quote(author: 'Jill Konrath',text: 'What differentiates sellers today is their ability to bring fresh ideas'),
+    Quote(author: 'Og Mandino',text: 'Always do your best. What you plant now, you will harvest later.'),
   ];
+
+  Widget quoteTemplate(quote){
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[800],
+              ),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[600],
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[400],
       appBar: AppBar(
-        title: Text('Motivational Gym Quotes'),
+        title: Text('Motivational Quotes'),
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       )
     );
   }
